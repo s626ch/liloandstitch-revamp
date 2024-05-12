9cb21361dbba9b9b5d958bee3950dc16cbc74122
stringsplit <77242831+nbitzz@users.noreply.github.com>
2024-05-12 16:32:53 -0700

    CSS fixes required

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import { remarkCustomEmoji } from './src/remark-plugins/customEmoji';
 // import { remarkCustomEmoji } from './src/remark-plugins/customEmoji';
 
 import mdx from "@astrojs/mdx";
@@ -8,10 +9,10 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://liloandstit.ch/',
-  markdown: {/*
-             remarkPlugins: [
+  markdown: {
+        remarkPlugins: [
              remarkCustomEmoji
-             ]*/ // @ me if you want custom emoji support and i'll add my plugin FAGGOT
+        ]
   },
   integrations: [mdx(), svelte()]
 });
--- src/components/LfmWidget.svelte
+++ src/components/LfmWidget.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+    import { onMount } from "svelte";
 
     interface TrackSkeletonUnparsed {
         image: {size: "large", "#text": string}[],
